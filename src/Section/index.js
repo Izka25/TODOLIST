574ba8beffd0113dda07d2982e76be5e4754a26b
Izabela Janusz <afrodyzjak100@gmail.com>
2024-05-02 03:38:50 +0200

    typo correction with extraHeaferContent on extraHeaderContent

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,10 +1,10 @@
 import "./style.css";
 
-const Section = ({ title, body, extraHeaferContent }) => (
+const Section = ({ title, body, extraHeaderContent }) => (
   <section className="section">
     <header className="section__header">
       <h2 className="section__title">{title}</h2>
-      {extraHeaferContent}
+      {extraHeaderContent}
     </header>
     <div className="section__body">
       {body}
