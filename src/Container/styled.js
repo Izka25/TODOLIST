dee37e0c5d26aa4353b90631426c38f8f469bdfe
Izabela Janusz <afrodyzjak100@gmail.com>
2025-11-09 01:08:44 +0100

    adding styled components to Container, creating styled.js and removing style.css

---
+++ src/Container/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+    font-family: "Montserrat", sans-serif;
+    line-height: 1.5;
+    text-align: left;
+    max-width: 900px;
+    margin: 10px;
+`;
