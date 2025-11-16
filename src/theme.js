24ce6be1f454ef243c566b80a10e28813772b1e8
Izabela Janusz <afrodyzjak100@gmail.com>
2025-11-16 21:03:09 +0100

    adding theme and ThemeProvider in index.js and adding theme file

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+export const theme = {
+    color: {
+        keppel: "#44a7a7",
+        silver: "#cccccc",
+        white: "#FFFFFF",
+        calypso: "#326C86",
+        gallery: "#eeeeee",
+        stealBlue: "#4390A8",
+        royalBlue: "#593ee0",
+        japaneseLaurel: "#008000",
+        apple: "#40C740",
+        red: "#FF0000",
+        scarlet: "#FE1F16",
+        softPeach: "#f3ebeb",
+    }
+}
