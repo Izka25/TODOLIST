e79c329220913bc2fcf0c10a2e17232cdbbc189b
Izabela Janusz <afrodyzjak100@gmail.com>
2025-11-16 21:31:12 +0100

    adding GlobalStyle file and removing style.css and adding GlobalStyle to index.js

---
+++ src/GlobalStyle.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+  box-sizing: border-box;
+}
+
+*,
+::after,
+::before {
+  box-sizing: inherit;
+}
+
+body {
+  font-family: "Lato", sans-serif;
+  background: ${({ theme }) => theme.color.softPeach};
+  max-width: 850px;
+  margin: 0 auto;
+  padding: 10px;
+}
+
+`;
