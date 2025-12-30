9189879ade4679a58ca094dd45a6336c91d05beb
Izabela Janusz <afrodyzjak100@gmail.com>
2025-12-30 10:30:40 +0100

    creating a new one App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,29 @@
-import  Tasks  from "./features/tasks/Tasks";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom/cjs/react-router-dom.min";
+import Navigation from "./Navigation";
+import { toAuthor, toTask, toTasks } from "./routes";
+import Task from "./features/tasks/TaskPage";
+import Tasks from "./features/tasks/TasksPage";
+import Author from "./features/author/AuthorPage";
 
-export default () => <Tasks />;
+
+export default () => (
+  <HashRouter>
+    <Navigation />
+    
+      <Switch>
+        <Route path={toTask()}>
+          <Task />
+        </Route>
+        <Route path={toTasks()}>
+          <Tasks />
+        </Route>
+        <Route path={toAuthor()}>
+          <Author />
+        </Route>
+        <Route>
+          <Redirect to={toTasks()} />
+        </Route>
+      </Switch>
+    
+  </HashRouter>
+);
