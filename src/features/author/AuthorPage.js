d891ffc05c1de081865fa3ffa2ef1162d7b78b57
Izabela Janusz <afrodyzjak100@gmail.com>
2025-12-30 10:38:57 +0100

    adding AuthorPage

---
+++ src/features/author/AuthorPage.js
@@ -0,0 +1,19 @@
+import Container from "../../common/Container";
+import Header from "../../common/Header";
+import Section from "../../common/Section";
+
+
+export default () => (
+    <Container>
+      <Header title="O autorze" />
+<Section
+        title="Izabela Janusz"
+        body={
+        <> 
+        <p>Jedną z moich pasji jest masaż, lubie pomagać ludziom i ich leczyć</p>
+        <p>Kocham podróżę, obecnie uczę sie hiszpańskiego oraz programowania  </p>
+        </>
+        }
+      />
+      </Container>
+);
