07d12fa9008d6198ea1cf7280900773d4db70b8d
Izabela Janusz <afrodyzjak100@gmail.com>
2025-12-30 10:58:04 +0100

    adding TaskList

---
+++ src/features/tasks/TasksPage/TaskList/index.js
@@ -0,0 +1,40 @@
+import { useSelector, useDispatch } from "react-redux";
+import { List, Task, Content, Button } from "./styled";
+import { toggleTaskDone, removeTask, selectHideDone, selectTasksByQuery } from "../../tasksSlice";
+import { Link, useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import searchQueryParamName from "../searchQueryParamName";
+import { toTask } from "../../../../routes";
+
+
+
+const TaskList = () => {
+const location = useLocation();
+  const query =(new URLSearchParams(location.search)).get(searchQueryParamName); 
+   
+
+  const tasks = useSelector(state => selectTasksByQuery(state, query));
+  const hideDone = useSelector(selectHideDone);
+  
+  const dispatch = useDispatch();
+
+  return (
+  <List>
+    {tasks.map((task) => (
+      <Task key={task.id} hidden={task.done && hideDone}>
+        <Button $toggleDone onClick={() => dispatch(toggleTaskDone(task.id))}>
+          {task.done ? "✔" : ""}
+        </Button>
+
+        <Content $done={task.done}>
+          <Link to={toTask({ id: task.id })}>{task.content}</Link>
+        </Content>
+
+        <Button $remove onClick={() => dispatch(removeTask(task.id))}>
+          🗑
+        </Button>
+      </Task>
+    ))}
+  </List>
+);
+}
+export default TaskList;
